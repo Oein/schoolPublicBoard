5f86ffa49e2a40ed44875b81d003cf5362380d90
Oein <oein0219@gmail.com>
2023-03-19 16:06:03 +0900

    init layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import type { AppProps } from "next/app";
+import Transition from "@/components/Transition";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Transition>
+      <Component {...pageProps} />
+      <ToastContainer newestOnTop position="bottom-right" />
+    </Transition>
+  );
 }
