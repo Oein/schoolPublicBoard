d2b71010488d0d0aab455e06c0d580e138036686
Oein <oein0219@gmail.com>
2023-03-20 20:03:46 +0900

    pwa

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+const withPlugins = require("next-compose-plugins");
+const withPWA = require("next-pwa");
+
 const nextConfig = {
   reactStrictMode: false,
 };
 
-module.exports = nextConfig;
+module.exports = withPWA({
+  dest: "/public",
+})(nextConfig);
