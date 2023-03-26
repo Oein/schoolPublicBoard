cca9cc3ef613481e038aeca9df61efc22a17bdd6
Oein <oein0219@gmail.com>
2023-03-26 16:13:16 +0900

    goto / when it is success

--- pages/adminProve.tsx
+++ pages/adminProve.tsx
@@ -1,10 +1,14 @@
 import Header from "@/components/Header";
 import axios from "axios";
+import { useRouter } from "next/router";
 import { useState } from "react";
 import { toast } from "react-toastify";
 
 export default function Admin() {
   let [val, setVal] = useState("");
+
+  const router = useRouter();
+
   return (
     <>
       <Header to="/" title="관리자 인증하기" />
@@ -23,6 +27,7 @@ export default function Admin() {
               let { data } = v;
               if (data.e) return toast.error(data.e);
               toast.success("인증 성공했습니다!");
+              router.push("/");
             });
           }}
         >
