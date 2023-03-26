28a823b1dd69d4aaae56a156e39ba171dceaf4b1
Oein <oein0219@gmail.com>
2023-03-26 16:14:57 +0900

    loading

--- pages/adminProve.tsx
+++ pages/adminProve.tsx
@@ -1,16 +1,20 @@
+import FullsizeLoading from "@/components/FullSizeLoading";
 import Header from "@/components/Header";
 import axios from "axios";
 import { useRouter } from "next/router";
+import nProgress from "nprogress";
 import { useState } from "react";
 import { toast } from "react-toastify";
 
 export default function Admin() {
   let [val, setVal] = useState("");
+  let [loading, setLoading] = useState(false);
 
   const router = useRouter();
 
   return (
     <>
+      <FullsizeLoading loading={loading} />
       <Header to="/" title="관리자 인증하기" />
       <div className="container">
         <input
@@ -23,12 +27,20 @@ export default function Admin() {
         <div
           className="button"
           onClick={() => {
-            axios.get(`/api/adminProver?pw=${val}`).then((v) => {
-              let { data } = v;
-              if (data.e) return toast.error(data.e);
-              toast.success("인증 성공했습니다!");
-              router.push("/");
-            });
+            setLoading(true);
+            nProgress.start();
+            axios
+              .get(`/api/adminProver?pw=${val}`)
+              .then((v) => {
+                let { data } = v;
+                if (data.e) return toast.error(data.e);
+                toast.success("인증 성공했습니다!");
+                router.push("/");
+              })
+              .finally(() => {
+                setLoading(false);
+                nProgress.done();
+              });
           }}
         >
           인증
